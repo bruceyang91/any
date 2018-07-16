3d13d8c8f112c38fae630215f8a99465e4eea443
ymc <ymc>
2018-07-16 23:17:50 +0800

    'test3'

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console":["error",{
+           "allow":["warn","error","info"]
+        }],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "parser":"babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "globals":{
+
+    },
+    "env": {
+        "browser": true,
+        "commonjs": true
+    }
+};
